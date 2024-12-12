4512d1e58ab66c37f87c330ef6d90c32732238bb
tettehnarh <leslienarh@outlook.com>
2024-12-12 00:23:57 +0000

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1 className='text-3xl text-white font-black'>Welcome to the world of NextJS!!</h1>
+      <h1 className="text-3xl text-white font-black">
+        Welcome to the world of NextJS!!
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
